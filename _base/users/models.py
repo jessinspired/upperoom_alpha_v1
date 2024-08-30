77882c7b0c4a42233afe9ce8632e062a7f096956
Frankin Obasi <franklinobasy@gmail.com>
2024-08-30 10:39:05 +0100

    Refactored users.models.py

--- _base/users/models.py
+++ _base/users/models.py
@@ -1,5 +1,4 @@
 from core.models import BaseModel
-
 from django.db import models
 from django.contrib.auth.models import (
     AbstractUser,
@@ -10,65 +9,39 @@ from django.contrib.auth.models import (
 
 
 class User(BaseModel, AbstractUser):
-    '''defines our custom AUTH_USER_MODEL
-    (see settings.py; line 137)
-    Args:
-        AbstractUser (AbstractUser): custom User parent
-    '''
+    """Defines our custom AUTH_USER_MODEL."""
     email = models.EmailField(unique=True)
 
     class Role(models.TextChoices):
-        '''defines roles for users
-        Args:
-            models (TextChoices): Defines the role choices for users
-        '''
         DEFAULT = 'DEFAULT', 'Default'
         CLIENT = 'CLIENT', 'Client'
         CREATOR = 'CREATOR', 'Creator'
 
-    # Role.choices here constrains user to only select one of these
-    # listed choices.
     role = models.CharField(
         max_length=50,
         choices=Role.choices,
         default=Role.DEFAULT
     )
 
-    first_name = models.CharField(
-        max_length=50,
-        null=False
-    )
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
 
-    last_name = models.CharField(
-        max_length=50,
-        null=False
-    )
+    def save(self, *args, **kwargs):
+        if not self.role:
+            self.role = self.Role.DEFAULT
+        super().save(*args, **kwargs)
 
 
-# creator models
+# Creator models
 class CreatorManager(BaseUserManager):
-    '''manager for creator proxy table
-    Args:
-        BaseUserManager (BaseUserManager): Creator manager
-    '''
+    """Manager for creator proxy table."""
 
     def get_queryset(self, *args, **kwargs):
-        '''helps query data belonging to users with role creator
-        Returns:
-            Creator: creator objects from user table
-        '''
-        results = super().get_queryset(*args, **kwargs)
-        return results.filter(role=User.Role.CREATOR)
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.CREATOR)
 
 
 class Creator(User):
-    '''Defines the creator proxy model
-    Args:
-        User (User): Custom AUTH_USER_MODEL
-    Returns:
-        Creator: created user(creator) object
-    '''
-    base_role = User.Role.CREATOR
+    """Defines the creator proxy model."""
 
     objects = CreatorManager()
 
@@ -76,110 +49,80 @@ class Creator(User):
         proxy = True
 
     def welcome(self):
-        return "only for creators"
+        return "Only for creators"
 
     def save(self, *args, **kwargs):
-        '''saves creator object and adds it to group; creators
-        '''
+        """Saves creator object and adds it to the 'creators' group."""
+        self.role = User.Role.CREATOR
         super().save(*args, **kwargs)
 
         creators_group, created = Group.objects.get_or_create(name="creators")
 
-        # add group permissions here
-
-        self.groups.add(creators_group)
+        if not self.groups.filter(name="creators").exists():
+            self.groups.add(creators_group)
 
     def delete(self, *args, **kwargs):
-        '''removes creator object from group before calling parent delete method
-        This is done to prevent a foreign key constraint with the groups table
-        '''
+        """Removes creator from group before deleting."""
         self.groups.remove(Group.objects.get(name='creators'))
         super().delete(*args, **kwargs)
 
 
 class CreatorProfile(BaseModel):
-    '''defines attributes and methods for creator profile
-    '''
+    """Defines attributes and methods for creator profile."""
     user = models.OneToOneField(
         Creator,
         on_delete=models.CASCADE,
         related_name='creator_profile'
     )
-
-    phone_number = models.CharField(
-        max_length=50,
-        null=True,
-        blank=False
-    )
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} profile"
 
 
-# for clients
+# Client models
 class ClientManager(BaseUserManager):
-    '''manager for client proxy table
-    Args:
-        BaseUserManager (BaseUserManager): Client manager
-    '''
+    """Manager for client proxy table."""
 
     def get_queryset(self, *args, **kwargs):
-        '''helps query data belonging to users with role Client
-        Returns:
-            Client: client objects from user table
-        '''
-        results = super().get_queryset(*args, **kwargs)
-        return results.filter(role=User.Role.CLIENT)
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.CLIENT)
 
 
 class Client(User):
-    '''Defines the Client proxy model
-    Args:
-        User (User): Custom AUTH_USER_MODEL
-    Returns:
-        Client: created user(client) object
-    '''
+    """Defines the Client proxy model."""
+
+    objects = ClientManager()
 
     class Meta:
         proxy = True
 
     def welcome(self):
-        return "only for clients"
+        return "Only for clients"
 
     def save(self, *args, **kwargs):
-        '''saves client object and adds it to group; clients
-        '''
+        """Saves client object and adds it to the 'clients' group."""
+        self.role = User.Role.CLIENT
         super().save(*args, **kwargs)
 
         clients_group, created = Group.objects.get_or_create(name="clients")
 
-        # add group permissions here
-
-        self.groups.add(clients_group)
+        if not self.groups.filter(name="clients").exists():
+            self.groups.add(clients_group)
 
     def delete(self, *args, **kwargs):
-        '''removes client object from group before calling parent delete method
-        This is done to prevent a foreign key constraint with the groups table
-        '''
+        """Removes client from group before deleting."""
         self.groups.remove(Group.objects.get(name='clients'))
         super().delete(*args, **kwargs)
 
 
 class ClientProfile(BaseModel):
-    '''defines methods and attributes for Client
-    '''
+    """Defines methods and attributes for Client."""
     user = models.OneToOneField(
         Client,
         on_delete=models.CASCADE,
         related_name='client_profile'
     )
-    phone_number = models.CharField(
-        max_length=50,
-        null=True,
-        blank=False
-    )
-
-    # add more client profile data
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} profile"
