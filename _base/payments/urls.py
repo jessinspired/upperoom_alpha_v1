9d2a35bd0c61429aeb5e7bf62a8e469d92f218c5
Obelem <ibbraidejesse@gmail.com>
2024-09-01 14:27:45 +0100

    testing live webhook --> added trailing slash from url

--- _base/payments/urls.py
+++ _base/payments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         views.initialize_transaction,
         name='initialize_transaction'
     ),
-    path('webhook', views.webhook_view, name='webhook'),
+    path('webhook/', views.webhook_view, name='webhook'),
     path('verify_payment/', views.verify_payment, name='verify_payment'),
 
 ]
