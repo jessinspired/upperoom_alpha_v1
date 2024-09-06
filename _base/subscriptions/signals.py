fd3286d07845044fa0f758a9b81b185f0c7f23ff
Obelem <ibbraidejesse@gmail.com>
2024-09-06 16:26:23 +0100

    added scheduled task change to verified for subscribed listing

--- _base/subscriptions/signals.py
+++ _base/subscriptions/signals.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 """signals for subscription models"""
 from django.db.models.signals import (
-    pre_save,
+    pre_save, post_save
 )
 from django.dispatch import receiver
 from .models import RoomProfile
 import logging
 from messaging.tasks import send_vacancy_update_mail
 from .models import Subscription, SubscribedListing
-
+from .tasks import change_status_to_verified
 
 logger = logging.getLogger('subscriptions')
 
 
 @receiver(pre_save, sender=RoomProfile)
-def process_vacancy(sender, instance, **kwargs):
+def process_vacancy(sender, instance, created, **kwargs):
     logger.info(
         f'debugging process vacancy')
 
-    if not instance.pk:
+    if created:
         if instance.is_vacant == False:
             return
 
@@ -55,3 +55,15 @@ def process_vacancy(sender, instance, **kwargs):
         send_vacancy_update_mail(instance.pk)
     except RoomProfile.DoesNotExist as e:
         logger.error(f'Room profile update failed with exception: {e}')
+
+
+@receiver(post_save, sender=SubscribedListing)
+def schedule_status_change(sender, instance, created, **kwargs):
+    logger.info('Begin subscribed lisiting scheduling')
+    if created:
+        logger.info(
+            f'Calling change status to verified function for subscribed listing {instance.pk}')
+        change_status_to_verified.apply_async(
+            args=[instance.id],
+            countdown=600  # 600s - 10 minutes
+        )
---
+++ _base/subscriptions/tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from celery import shared_task
+from django.utils import timezone
+from .models import SubscribedListing
+import logging
+
+logger = logging.getLogger('subscriptions')
+
+
+@shared_task
+def change_status_to_verified(subscribed_listing_id):
+    try:
+        subscribed_listing = SubscribedListing.objects.get(
+            id=subscribed_listing_id)
+
+        if subscribed_listing.status == SubscribedListing.Status.UNVERIFIED:
+            subscribed_listing.status = SubscribedListing.Status.VERIFIED
+            subscribed_listing.save()
+            logger.info(
+                f'Status changed to verified for subscribed_listing with id {subscribed_listing_id}')
+        else:
+            logger.info(
+                f'Cannot verify lodge with status: {subscribed_listing.status}')
+    except SubscribedListing.DoesNotExist:
+        logger.error(
+            f'subscribed_listing does not exist for id: {subscribed_listing_id}')
