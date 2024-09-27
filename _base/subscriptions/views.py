5daa57afd5206b5bd87a2294aee9af494fee6009
Obelem <ibbraidejesse@gmail.com>
2024-09-27 18:39:06 +0100

    changed the randomnization

--- _base/subscriptions/views.py
+++ _base/subscriptions/views.py
@@ -122,7 +122,7 @@ def subscription_algorithm(regions, subscription):
         subquery = Lodge.objects.filter(
             group=OuterRef('group'),
             room_profiles__vacancy__gt=0  # Ensure vacancy for the selected lodge
-        ).order_by(Random()).values('pk')[:1]
+        ).order_by('?').values('pk')[:1]
 
         lodges = lodges_in_region.filter(
             Exists(subquery)
