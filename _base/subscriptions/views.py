e6676ca7a2a09d615026925315fc59bf471a455d
Obelem <ibbraidejesse@gmail.com>
2024-09-27 19:00:58 +0100

    dubugging subquery__3

--- _base/subscriptions/views.py
+++ _base/subscriptions/views.py
@@ -124,8 +124,6 @@ def subscription_algorithm(regions, subscription):
             room_profiles__vacancy__gt=0  # Ensure vacancy for the selected lodge
         ).order_by(Random()).values('pk')[:1]
 
-        logger.info(f'subquery: {str(subquery.query)}')
-
         lodges = lodges_in_region.filter(
             Exists(subquery)
         ).distinct('group')
