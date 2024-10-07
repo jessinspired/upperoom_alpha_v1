e93bbe2f928d0bcae503f250d1885950a6ffd3c2
Obelem <ibbraidejesse@gmail.com>
2024-10-07 11:46:20 +0100

    debug set lodge locaiton_2

--- _base/listings/static/listings/scripts/setLodgeLocation.js
+++ _base/listings/static/listings/scripts/setLodgeLocation.js
@@ -12,17 +12,21 @@ function getCoordinates(event) {
       let latitude = position.coords.latitude;
       let longitude = position.coords.longitude;
 
-      htmx.ajax("POST", `$https://upperoom.ng{event.target.getAttribute("data-url")}`, {
-        headers: {
-          "X-CSRFToken": csrfToken,
-        },
-        target: "#location-handler",
-        swap: "outerHTML",
-        values: {
-          latitude: latitude,
-          longitude: longitude,
-        },
-      });
+      htmx.ajax(
+        "POST",
+        `https://upperoom.ng${event.target.getAttribute("data-url")}`,
+        {
+          headers: {
+            "X-CSRFToken": csrfToken,
+          },
+          target: "#location-handler",
+          swap: "outerHTML",
+          values: {
+            latitude: latitude,
+            longitude: longitude,
+          },
+        }
+      );
     },
     (error) => {
       console.error("Error getting location: ", error);
